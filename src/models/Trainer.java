7edc261e01cbea50f1986aeba70e3afb312dc05c
Devia-07 <devia.sebastian@univalle.edu.co>
2025-03-21 21:54:07 -0500

    implementacion de la clase Trainer 0.1

--- src/models/Trainer.java
+++ src/models/Trainer.java
@@ -1,4 +1,55 @@
 package models;
+import models.pokemons.Pokemon;
+import java.util.Scanner;
+
+/**
+ * @author Sebastian Devia
+ */
 
 public class Trainer {
+    /**
+     * atributos: NombreEntrenador,Equipo
+     */
+    public String nameTrainer;
+    private Pokemon[] team;
+
+    public Trainer(String nameTrainer, Pokemon[] team) {
+        this.nameTrainer = nameTrainer;
+        team = team;
+    }
+
+    public String getNameTrainer() {
+        return nameTrainer;
+    }
+
+    public void setNameTrainer(String nameTrainer) {
+        this.nameTrainer = nameTrainer;
+    }
+
+    public Pokemon[] getTeam() {
+        return team;
+    }
+
+    public void setTeam(String[] team) {
+        team = team;
+    }
+
+
+
+    /**
+     * Metodo para eligir un pokemon de batalla
+    * @return el pokemon escogido
+     */
+    public Pokemon SelectPokemon() {
+        Scanner sc = new Scanner(System.in);
+        //mostramos los pokemones
+        for (int i = 0; i < team.length; i++) {
+            System.out.printf("%d .%s \n", i + 1, team[i]);
+
+        }
+        System.out.print("Selecciona un pokemon para combatir: ");
+        int PokemonChosen = sc.nextInt()-1;
+        return team[PokemonChosen];
+
+    }
 }
