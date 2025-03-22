01b0125f64b38c043c2156ea35d0138a9f434402
simon4585 <simon.tarazona@correounivalle.edu.co>
2025-03-21 21:58:53 -0500

    implementacion de la clase ataque + correccion

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,11 @@
 package main;
 import models.pokemons.utils.Attack;
-import models.pokemons.utils.TypeAttack;
 import models.pokemons.utils.TypeDamage;
 
 public class Main {
     public static void main(String[] args) {
-        Attack ataque1 = new Attack("Llamarada", (short)110, TypeDamage.SPECIAL, TypeAttack.FIRE);
-        Attack ataque2 = new Attack("Terremoto", (short)100, TypeDamage.PHYSICAL, TypeAttack.PLANT);
+        Attack ataque1 = new Attack("Llamarada", (short)110, TypeDamage.SPECIAL);
+        Attack ataque2 = new Attack("Terremoto", (short)100, TypeDamage.PHYSICAL);
 
         ataque1.showInfo();
         ataque2.showInfo();
--- src/models/pokemons/utils/Attack.java
+++ src/models/pokemons/utils/Attack.java
@@ -4,13 +4,12 @@ public class Attack {
     private String name;          // Nombre del ataque
     private short power;            // Poder del ataque
     private TypeDamage typeDamage;      // Tipo de ataque (FISICO o ESPECIAL
-    private TypeAttack typeAttack;
 
-    public Attack(String name, short power, TypeDamage typeDamage, TypeAttack typeAttack) {
+    public Attack(String name, short power, TypeDamage typeDamage) {
         this.name = name;
         this.power = power;
         this.typeDamage = typeDamage;
-        this.typeAttack = typeAttack;
+
     }
 
     public String getName() {
@@ -37,19 +36,13 @@ public class Attack {
         this.typeDamage = typeDamage;
     }
 
-    public TypeAttack getTypeAttack() {
-        return typeAttack;
-    }
 
-    public void setTypeAttack(TypeAttack typeAttack) {
-        this.typeAttack = typeAttack;
-    }
 
     public void showInfo() {
         System.out.println("Ataque: " + name);
         System.out.println("Poder: " + power);
         System.out.println("Tipo de Daño: " + typeDamage);
-        System.out.println("Tipo de Ataque: " + typeAttack);
+
     }
 }
 
