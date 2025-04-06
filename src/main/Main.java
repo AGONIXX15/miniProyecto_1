a130e752ef6dff1073f5c91b178774ff5dbcf675
agonixx <rubio.sebastian@correounivalle.edu.co>
2025-04-05 20:22:24 -0500

    fix: the system of combat in pokemon has been made

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,17 @@
 package main;
 import models.pokemon.utils.Attack;
 import models.pokemon.utils.TypeDamage;
-import models.pokemon.WaterPokemon;
+import models.pokemon.Pokemon;
+import models.pokemon.utils.TypePokemon;
 
 public class Main {
     public static void main(String[] args) {
         Attack ataque1 = new Attack("Llamarada", (short)110, TypeDamage.SPECIAL);
         Attack ataque2 = new Attack("Terremoto", (short)100, TypeDamage.PHYSICAL);
-        WaterPokemon w1 = new WaterPokemon("squirtle",100, new Attack[]{ataque1,ataque2});
-        WaterPokemon w2 = new WaterPokemon("popo",100, new Attack[]{ataque1,ataque2});
-        w1.useAttack(w2, ataque1);
-
+        Pokemon w1 = new Pokemon("squirtle",100, TypePokemon.WATER, new Attack[]{ataque1,ataque2});
+        Pokemon pikachu = new Pokemon("pikachu", 100, TypePokemon.ELECTRIC, new Attack[]{ataque1});
+        w1.makeDamage(pikachu,ataque1);
+        System.exit(0);
     }
 }
 
--- src/models/pokemon/Pokemon.java
+++ src/models/pokemon/Pokemon.java
@@ -3,24 +3,27 @@ package models.pokemon;
 import models.pokemon.utils.Attack;
 import models.pokemon.utils.TypePokemon;
 
+import java.util.Arrays;
+
 public class Pokemon {
     private String name;
     private TypePokemon type;
     private Attack[] attacks;
+    private int healthMax;
     private int health;
-
 
     /**
      * constructor de la clase abstracta pokemon
      *
      * @param name    nombre del pokemon
-     * @param health  salud del pokemon
+     * @param healthMax  salud maxima del pokemon
      * @param type    tipo del pokemon
      * @param attacks ataques del pokemon
      */
-    public Pokemon(String name, int health, TypePokemon type, Attack[] attacks) {
+    public Pokemon(String name, int healthMax, TypePokemon type, Attack[] attacks) {
         this.name = name;
-        this.health = health;
+        this.healthMax = healthMax;
+        this.health = healthMax;
         this.type = type;
         this.attacks = attacks;
     }
@@ -28,6 +31,14 @@ public class Pokemon {
 
     public String getName() {
         return name;
+    }
+
+    public int getHealthMax() {
+        return healthMax;
+    }
+
+    public void setHealthMax(int healthMax) {
+        this.healthMax = healthMax;
     }
 
     public int getHealth() {
@@ -56,5 +67,40 @@ public class Pokemon {
 
     public void setType(TypePokemon type) {
         this.type = type;
+    }
+
+    public void cure(int health){
+        this.health = Math.min(this.health + health, this.healthMax);
+        System.out.printf("%s ha sido curado hasta %d\n", this.name, this.health);
+    }
+
+    public void takeDamage(int damage) {
+        health = Math.max(0, health-damage);
+        System.out.printf("%s recibio %d de daño \n", name, damage);
+        System.out.printf("%s tiene una vida de %d \n", name, health);
+        if (health == 0){
+            System.out.printf("%s ha sido derrotado...\n", name);
+        }
+    }
+
+    public boolean hasAdvantage(Pokemon enemy){
+       if(Arrays.binarySearch(type.strong, enemy.toString()) >= 0){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isAlive(){
+        return health > 0;
+    }
+
+    public void makeDamage(Pokemon enemy, Attack attack){
+        float advantage = (hasAdvantage(enemy)) ? 1.3f : 1;
+        if(advantage > 1){
+            System.out.println("el ataque ha sido efectivo!!");
+        }
+        int damage = (int) advantage * attack.getPower();
+        System.out.printf("%s realizo %s hacia %s con un daño de %d\n",name, attack.getName(), enemy.getName(),damage);
+        enemy.takeDamage(damage);
     }
 }
